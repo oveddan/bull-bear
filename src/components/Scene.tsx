285a3959ce279aba59bd9c0c76355a851a9d28b4
Dan Oved <stangogh@gmail.com>
2023-03-01 22:18:11 -0700

    created basic scene and loading behave grpah, but cant run graph cause of big loop

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,88 +1,79 @@
 import { OrbitControls, Stage, useGLTF } from '@react-three/drei';
 import { Canvas, ObjectMap } from '@react-three/fiber';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Object3D } from 'three';
 import catModel from '../assets/final_cat.glb';
 import { useScene } from '../scene/useScene';
 import { GLTF } from 'three-stdlib';
 import {
-  useBehaveGraphFlow,
   useCoreRegistry,
-  useCustomNodeTypes,
   useDependency,
   useGraphRunner,
-  useMergeDependencies,
-  useNodeSpecJson
+  useMergeDependencies
 } from '@oveddan-behave-graph/flow';
 import { useGraphJson } from '../scene/buildGraphJson';
 import { useSceneRegistry } from '../hooks/useSceneRegistry';
+import { createSceneDependency } from '@oveddan-behave-graph/scene';
+import ToggleAnimations from '../scene/ToggleAnimations';
+import { RegisterOnClickListeners } from '../scene/RegisterOnClickListeners';
 
 export const Scene = () => {
   const gltf = useGLTF(catModel) as GLTF & ObjectMap;
 
+  // todo: combine the below into a single hook
   const {
     nodeDefinitions: coreNodeDefinitions,
     valuesDefinitions: coreValueDefinitions,
     dependencies: coreDependencies
   } = useCoreRegistry();
 
-  const { nodeDefinitions, valuesDefinitions } = useSceneRegistry({
+  // get the possible nodes that can be defined for the behavior graph,
+  // by merging the core nodes with the scene nodes
+  const { nodeDefinitions /*, valuesDefinitions*/ } = useSceneRegistry({
     existingNodeDefinitions: coreNodeDefinitions,
     existingValuesDefinitions: coreValueDefinitions
   });
 
-  const { scene, animations, sceneOnClickListeners } = useScene(gltf);
+  // create the scene api on the loaded gltf, that is used by the behavior graph
+  const {
+    scene,
+    animations,
+    sceneOnClickListeners: onClickListeners
+  } = useScene(gltf);
 
+  // create the scene dependency, a wrapper around the scene api
+  // created around the gltf, that is used by the behavior graph
+  // scene nodes
   const sceneDependency = useDependency(scene, createSceneDependency);
+  // merge the core dependencies with the scene dependency
   const dependencies = useMergeDependencies(coreDependencies, sceneDependency);
 
-  const specJson = useNodeSpecJson({
-    dependencies,
-    nodes: nodeDefinitions,
-    values: valuesDefinitions
-  });
+  // loads the behave graph for the scene
+  const graphJson = useGraphJson();
 
-  const initialGraphJson = useGraphJson();
-
-  const {
-    nodes,
-    edges,
-    onNodesChange,
-    onEdgesChange,
-    graphJson,
-    setGraphJson
-  } = useBehaveGraphFlow({
-    initialGraphJson,
-    specJson
-  });
-
-  useGraphRunner({
-    graphJson,
-    valueTypeDefinitions: coreValueDefinitions,
-    eventEmitter: coreDependencies.lifecycleEventEmitter,
-    dependencies,
-    nodeDefinitions,
-    autoRun: true
-  });
-
-  const customNodeTypes = useCustomNodeTypes({
-    specJson
-  });
-
-  const [refreshFlow, setRefreshFlow] = useState(false);
-
-  useEffect(() => {
-    setRefreshFlow(true);
-    setTimeout(() => setRefreshFlow(false));
-  }, [customNodeTypes]);
+  // runs the behavior graph on the scene
+  // useGraphRunner({
+  //   graphJson,
+  //   valueTypeDefinitions: coreValueDefinitions,
+  //   eventEmitter: coreDependencies.lifecycleEventEmitter,
+  //   dependencies,
+  //   nodeDefinitions,
+  //   autoRun: true
+  // });
 
   const [mainRef, setMainRef] = useState<Object3D | null>(null);
   return (
     <Canvas>
       <OrbitControls makeDefault target={mainRef?.position} />
       <Stage shadows intensity={1} environment="city" preset="rembrandt">
-        <primitive object={gltf.scene} ref={setMainRef} />
+        <primitive object={gltf.scene} ref={setMainRef}>
+          <RegisterOnClickListeners
+            gltf={gltf}
+            onClickListeners={onClickListeners}
+          />
+        </primitive>
       </Stage>
+      <ToggleAnimations gltf={gltf} animationsState={animations} />
     </Canvas>
   );
 };
